de47ca12aa1a9ccb73fb82cec8bf26ef1b4e87a8
Jason IBISI <jasonibisi@gmail.com>
2020-01-20 11:05:37 +0100

    Products Create View Dropdown

--- GraniteHouse/Models/ViewModel/ProductsViewModel.cs
+++ GraniteHouse/Models/ViewModel/ProductsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,9 @@ namespace GraniteHouse.Models.ViewModel
     public class ProductsViewModel
     {
         public Products Products { get; set; }
-
+        [Display(Name = "Product Types")]
         public IEnumerable<ProductTypes> ProductTypes { get; set; }
-
+        [Display(Name = "Special Tags")]
         public IEnumerable<SpecialTags> SpecialTags { get; set; }
     }
 }
