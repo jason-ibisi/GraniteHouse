c01a7a54e8364cab0e80b18f9531a657f7fdd349
Jason IBISI <jasonibisi@gmail.com>
2020-01-20 09:29:36 +0100

    Products Controller - Create action and view

--- GraniteHouse/Controllers/ProductsController.cs
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
 using GraniteHouse.Models.ViewModel;
+using GraniteHouse.Utility;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,13 +16,16 @@ namespace GraniteHouse.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
 
         [BindProperty]
         public ProductsViewModel ProductsVM { get; set; }
 
-        public ProductsController(ApplicationDbContext db)
+        public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
         {
             _db = db;
+            _hostingEnvironment = hostingEnvironment;
+
             ProductsVM = new ProductsViewModel()
             {
                 ProductTypes = _db.ProductTypes.ToList(),
@@ -32,6 +38,60 @@ namespace GraniteHouse.Controllers
         {
             var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
             return View(await products.ToListAsync());
+        }
+
+        //GET : Products Create
+        public IActionResult Create()
+        {
+            return View(ProductsVM);
+        }
+
+        //POST : Products Create
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProduct()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ProductsVM);
+            }
+
+            _db.Products.Add(ProductsVM.Products);
+            await _db.SaveChangesAsync();
+
+            // Add Image 
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            // Get product from DB
+            var productFromDb = _db.Products.Find(ProductsVM.Products.Id);
+
+            if(files.Count != 0)
+            {
+                // Image has been uploaded
+                var uploads = Path.Combine(webRootPath, StaticDetails.ImageFolder);
+                var extension = Path.GetExtension(files[0].FileName);
+
+                // Copy file to server
+                using (FileStream fileStream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+
+                // Update product in database with new path
+                productFromDb.Image = @"\" + StaticDetails.ImageFolder + @"\" + ProductsVM.Products.Id + extension;
+
+            }
+            else
+            {
+                // Use default image - user did not upload image
+                var uploads = Path.Combine(webRootPath, StaticDetails.ImageFolder + @"\" + StaticDetails.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + StaticDetails.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg");
+                productFromDb.Image = @"\" + StaticDetails.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg";
+            }
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ GraniteHouse/Utility/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Utility
+{
+    public class StaticDetails
+    {
+        public const string DefaultProductImage = "default_product_image.jpg";
+        public const string ImageFolder = @"image\ProductImage";
+    }
+}
