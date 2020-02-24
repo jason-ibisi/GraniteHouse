6ca222fcccc0110bf70f97346e5521ac60b16332
Jason IBISI <jasonibisi@gmail.com>
2020-02-24 15:44:46 +0100

    Users: Add Properties & Role Manager, Register Page, model and post action

--- GraniteHouse/Utility/StaticDetails.cs
+++ GraniteHouse/Utility/StaticDetails.cs
@@ -9,5 +9,7 @@ namespace GraniteHouse.Utility
     {
         public const string DefaultProductImage = "default_product_image.jpg";
         public const string ImageFolder = @"images\ProductImage";
+        public const string AdminEndUser = "Admin";
+        public const string SuperAdminEndUser = "Super Admin";
     }
 }
