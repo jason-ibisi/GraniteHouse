9f3503ef77960e0d3315b9db5f4ef71b9a78a764
Jason IBISI <jasonibisi@gmail.com>
2020-02-25 17:32:39 +0100

    Users: Admin Users Controller, Edit action and view

--- GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
+++ GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraniteHouse.Areas.Admin.Controllers
@@ -21,6 +22,48 @@ namespace GraniteHouse.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_db.ApplicationUsers.ToList());
+        }
+
+        //Get Edit Admin User
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+
+            var userFromDb = await _db.ApplicationUsers.FindAsync(id);
+
+            if(userFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFromDb);
+        }
+
+        // POST Edit Admin User
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, ApplicationUser applicationUser)
+        {
+            if(id != applicationUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = _db.ApplicationUsers.Where(u => u.Id == id).FirstOrDefault();
+                user.Name = applicationUser.Name;
+                user.PhoneNumber = applicationUser.PhoneNumber;
+                user.Email = applicationUser.Email;
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(applicationUser);
         }
     }
 }
